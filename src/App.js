d7151b317dd681725ccfc6ded8b904c27cde641b
DESKTOP-NPUVJIQ\Mark 1 <tabish169000@gmail.com>
2023-09-21 19:33:57 -0700

    initial project structre

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto py-12">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum inventore, quasi, explicabo ex amet quisquam numquam illum, aspernatur quod quis et. Blanditiis, ducimus aut voluptates debitis porro cumque ad ipsum!
     </div>
   );
 }
